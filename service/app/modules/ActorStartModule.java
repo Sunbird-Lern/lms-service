9669a0cbdafbae7eab820647a09e908ed6abf137
Rahul K <rahul.kumar@tarento.com>
2019-11-18 13:44:52 +0530

    actor start module changes

--- service/app/modules/ActorStartModule.java
+++ service/app/modules/ActorStartModule.java
@@ -10,11 +10,9 @@ public class ActorStartModule extends AbstractModule implements AkkaGuiceSupport
 
   @Override
   protected void configure() {
-    super.configure();
     System.out.println("binding actors for dependency injection");
     final RouterConfig config = new FromConfig();
     for (ACTOR_NAMES actor : ACTOR_NAMES.values()) {
-      System.out.println("binding " + actor.getActorClass() + "=>" + actor.getActorName());
       bindActor(
           actor.getActorClass(),
           actor.getActorName(),

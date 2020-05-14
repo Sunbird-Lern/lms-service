27c4d8adf7743799c76730afa6e025a65c78eeaf
NehaVerma <neha0305verma@gmail.com>
2020-05-15 00:05:49 +0530

    course create with unirest

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/course/CourseManagementActor.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/course/CourseManagementActor.java
@@ -1,21 +1,30 @@
 package org.sunbird.learner.actors.course;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.base.BaseActor;
-import org.sunbird.common.models.response.Response;
-import org.sunbird.common.models.util.HttpUtil;
+import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.response.Response;;
+import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.ExecutionContext;
 import org.sunbird.common.request.Request;
+import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.learner.util.Util;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.text.MessageFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.sunbird.common.models.util.JsonKey.EKSTEP_BASE_URL;
 import static org.sunbird.common.models.util.ProjectUtil.getConfigValue;
+import static org.sunbird.common.responsecode.ResponseCode.SERVER_ERROR;
 
 public class CourseManagementActor extends BaseActor {
     private static ObjectMapper mapper = new ObjectMapper();
+
     @Override
     public void onReceive(Request request) throws Throwable {
         Util.initializeContext(request, "COURSE_CREATE");
@@ -32,34 +41,81 @@ public class CourseManagementActor extends BaseActor {
         }
     }
 
-    private void createCourse(Request request) throws Exception{
-        String requestURL;
+    private void createCourse(Request request) throws Exception {
         Map<String, Object> contentMap = new HashMap<>();
-        contentMap.putAll((Map<String,Object>)request.get("course"));
-        if(request.getRequest().containsKey("source")){
+        if(null == request.get("course")){
+           ProjectCommonException.throwClientErrorException(ResponseCode.RESOURCE_NOT_FOUND, "Please provide course");
+        }
+        contentMap.putAll((Map<String, Object>) request.get("course"));
+        String requestUrl;
+        if (request.getRequest().containsKey("source")) {
             contentMap.put("copyScheme", "TextBookToCourse");
             contentMap.put("courseType", "CurriculumCourse");
-            requestURL = getConfigValue(EKSTEP_BASE_URL) + "/content/v3/copy/" + request.get("source") + "?type=deep";
+            requestUrl = getConfigValue(EKSTEP_BASE_URL) + "/content/v3/copy/" + request.get("source") + "?type=deep";
         } else {
             contentMap.put("courseType", "TrainingCourse");
-            requestURL = getConfigValue(EKSTEP_BASE_URL) + "/content/v3/create";
+            requestUrl = getConfigValue(EKSTEP_BASE_URL) + "/content/v3/create";
         }
-        String reqBody = "{\"request\": {\"content\": " + mapper.writeValueAsString(contentMap) + "}}";
-        Map<String, String> headers = new HashMap<String, String>(){{
+        Map<String, String> headers = new HashMap<String, String>() {{
             put("Content-Type", "application/json");
             put("X-Channel-Id", (String) request.getContext().get("channel"));
         }};
-        String res = HttpUtil.sendPostRequest(requestURL, reqBody, headers);
-        Response response = new Response();
-        Map<String, Object> responseMap = mapper.readValue(res, Map.class);
-        Map<String,Object> result = (Map<String,Object>) responseMap.get("result");
-        if(request.getRequest().containsKey("source")) {
-            Map<String,Object> node_id = (Map<String,Object>) result.get("node_id");
-            result.put("identifier", node_id.get(request.get("source")));
+        Map<String, Object> requestMap = new HashMap<String, Object>(){{
+            put("request", new HashMap<String, Object>() {{
+                put("content", contentMap);
+            }});
+        }};
+        try {
+            HttpResponse<String> updateResponse =
+                    Unirest.post(requestUrl)
+                            .headers(headers)
+                            .body(mapper.writeValueAsString(requestMap))
+                            .asString();
+
+            if (null != updateResponse) {
+                Response response = mapper.readValue(updateResponse.getBody(), Response.class);
+                if (response.getResponseCode().getResponseCode() == ResponseCode.OK.getResponseCode()) {
+                    if (request.getRequest().containsKey("source")) {
+                        Map<String, Object> node_id = (Map<String, Object>) response.get("node_id");
+                        response.put("identifier", node_id.get(request.get("source")));
+                    }
+                    response.getResult().remove("node_id");
+                    response.getResult().put("course_id", response.get("identifier"));
+                    response.getResult().put("identifier", response.get("identifier"));
+                    response.getResult().put("versionKey", response.get("versionKey"));
+                    sender().tell(response, self());
+                } else {
+                    Map<String, Object> resultMap =
+                            Optional.ofNullable(response.getResult()).orElse(new HashMap<>());
+                    String message = "Course creation failed ";
+                    if (MapUtils.isNotEmpty(resultMap)) {
+                        Object obj = Optional.ofNullable(resultMap.get(JsonKey.TB_MESSAGES)).orElse("");
+                        if (obj instanceof List) {
+                            message += ((List<String>) obj).stream().collect(Collectors.joining(";"));
+                        } else if(StringUtils.isNotEmpty(response.getParams().getErrmsg())){
+                            message += response.getParams().getErrmsg();
+                        } else {
+                            message += String.valueOf(obj);
+                        }
+                    }
+                    ProjectCommonException.throwClientErrorException(
+                            ResponseCode.customServerError,
+                            MessageFormat.format(
+                                    ResponseCode.customServerError.getErrorMessage(), message));
+                }
+            } else {
+                ProjectCommonException.throwClientErrorException(ResponseCode.CLIENT_ERROR);
+            }
+        } catch (Exception ex) {
+            if (ex instanceof ProjectCommonException) {
+                throw ex;
+            } else {
+                throw new ProjectCommonException(
+                        ResponseCode.CLIENT_ERROR.getErrorCode(),
+                        ResponseCode.CLIENT_ERROR.getErrorMessage(),
+                        SERVER_ERROR.getResponseCode());
+            }
         }
-        response.getResult().put("course_id", result.get("identifier"));
-        response.getResult().put("identifier", result.get("identifier"));
-        response.getResult().put("versionKey", result.get("versionKey"));
-        sender().tell(response, self());
     }
+
 }

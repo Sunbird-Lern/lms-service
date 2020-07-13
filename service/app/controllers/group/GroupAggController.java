3ed9d830d9c097488d7b5da55b2468043092caa4
amitpriyadarshi <darshi.1983@gmail.com>
2020-07-14 01:11:40 +0530

    Issue #SC-1812 feat: Group activity aggregates API - testcase

--- service/app/controllers/group/GroupAggController.java
+++ service/app/controllers/group/GroupAggController.java
@@ -2,11 +2,9 @@ package controllers.group;
 
 import akka.actor.ActorRef;
 import controllers.BaseController;
-import controllers.group.validator.GroupActivityValidator;
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
-import org.sunbird.common.request.HeaderParam;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.request.RequestValidator;
 import play.mvc.Http;

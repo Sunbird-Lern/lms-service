1723b3254f5dd92d2333013da6cbe64d920b58b4
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2020-07-27 14:32:02 +0530

    Issue #000 fix: to java object.

--- course-mw/enrolment-actor/src/main/scala/org/sunbird/group/GroupAggregatesActor.scala
+++ course-mw/enrolment-actor/src/main/scala/org/sunbird/group/GroupAggregatesActor.scala
@@ -100,7 +100,7 @@ class GroupAggregatesActor extends BaseActor {
       usersAggs.map(dbAggRecord => {
         val dbAgg = dbAggRecord.get("agg").asInstanceOf[java.util.Map[String, AnyRef]]
         val aggLastUpdated = dbAggRecord.get("agg_last_updated").asInstanceOf[java.util.Map[String, AnyRef]]
-        val agg = List(Map("metric"-> aggName, "value" -> dbAgg.get(aggName), "lastUpdatedOn" -> aggLastUpdated.get(aggName)))
+        val agg = List(Map("metric"-> aggName, "value" -> dbAgg.get(aggName), "lastUpdatedOn" -> aggLastUpdated.get(aggName)).asJava).asJava
         (membersMap.filterKeys(key => GROUP_MEMBERS_METADATA.contains(key)) ++ Map("agg" -> agg)).asJava
       }).toList
     } else List()

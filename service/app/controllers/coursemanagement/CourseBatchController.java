e67f8759986adf243f5debd44a606a51f996fa95
NehaVerma <neha0305verma@gmail.com>
2020-09-04 14:23:15 +0530

    collectionId change for update batch

--- service/app/controllers/coursemanagement/CourseBatchController.java
+++ service/app/controllers/coursemanagement/CourseBatchController.java
@@ -68,7 +68,10 @@ public class CourseBatchController extends BaseController {
         ActorOperations.UPDATE_BATCH.getValue(),
         httpRequest.body().asJson(),
         (request) -> {
-          new CourseBatchRequestValidator().validateUpdateCourseBatchRequest((Request) request);
+          Request req = (Request) request;
+          String courseId = req.getRequest().containsKey(JsonKey.COURSE_ID) ? JsonKey.COURSE_ID : JsonKey.COLLECTION_ID;
+          req.getRequest().put(JsonKey.COURSE_ID, req.getRequest().get(courseId));
+          new CourseBatchRequestValidator().validateUpdateCourseBatchRequest(req);
           return null;
         },
         httpRequest);

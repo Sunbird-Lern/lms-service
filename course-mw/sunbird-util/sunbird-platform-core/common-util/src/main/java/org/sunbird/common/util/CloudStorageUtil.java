2776ce89b4f216995589791b7f584ba44f79f872
PrasadMoka <prasad@sanketika.in>
2022-11-07 23:54:17 +0530

    LR-265 Cloud Agnostic changes

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/CloudStorageUtil.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/CloudStorageUtil.java
@@ -5,55 +5,19 @@ import java.util.Map;
 import org.sunbird.cloud.storage.IStorageService;
 import org.sunbird.cloud.storage.factory.StorageConfig;
 import org.sunbird.cloud.storage.factory.StorageServiceFactory;
-import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.ProjectUtil;
 import org.sunbird.common.models.util.PropertiesCache;
-import org.sunbird.common.responsecode.ResponseCode;
 import scala.Option;
 import scala.Some;
-
-import static org.sunbird.common.models.util.JsonKey.*;
 
 public class CloudStorageUtil {
   private static final int STORAGE_SERVICE_API_RETRY_COUNT = 3;
 
   private static final Map<String, IStorageService> storageServiceMap = new HashMap<>();
 
-  public enum CloudStorageType {
-    AZURE(AZURE_STR),
-    AWS(AWS_STR),
-    GCLOUD(GCLOUD_STR);
-
-    private String type;
-
-    private CloudStorageType(String type) {
-      this.type = type;
-    }
-
-    public String getType() {
-      return this.type;
-    }
-
-    public static CloudStorageType getByName(String type) {
-      if (AZURE.type.equalsIgnoreCase(type)) {
-        return CloudStorageType.AZURE;
-      } if (AWS.type.equalsIgnoreCase(type)) {
-        return CloudStorageType.AWS;
-      } if (GCLOUD.type.equalsIgnoreCase(type)) {
-        return CloudStorageType.GCLOUD;
-      } else {
-        ProjectCommonException.throwClientErrorException(
-            ResponseCode.errorUnsupportedCloudStorage,
-            ProjectUtil.formatMessage(
-                ResponseCode.errorUnsupportedCloudStorage.getErrorMessage(), type));
-        return null;
-      }
-    }
-  }
-
   public static String upload(
-      CloudStorageType storageType, String container, String objectKey, String filePath) {
+      String storageType, String container, String objectKey, String filePath) {
 
     IStorageService storageService = getStorageService(storageType);
 
@@ -68,20 +32,13 @@ public class CloudStorageUtil {
   }
 
   public static String getSignedUrl(
-      CloudStorageType storageType, String container, String objectKey) {
+      String storageType, String container, String objectKey) {
     IStorageService storageService = getStorageService(storageType);
-    return getSignedUrl(storageService, storageType, container, objectKey);
-  }
-
-  public static String getAnalyticsSignedUrl(
-      CloudStorageType storageType, String container, String objectKey) {
-    IStorageService analyticsStorageService = getAnalyticsStorageService(storageType);
-    return getSignedUrl(analyticsStorageService, storageType, container, objectKey);
+    return getSignedUrl(storageService, container, objectKey);
   }
 
   public static String getSignedUrl(
       IStorageService storageService,
-      CloudStorageType storageType,
       String container,
       String objectKey) {
     int timeoutInSeconds = getTimeoutInSeconds();
@@ -89,27 +46,21 @@ public class CloudStorageUtil {
         container, objectKey, Some.apply(timeoutInSeconds), Some.apply("r"));
   }
 
-  private static IStorageService getStorageService(CloudStorageType storageType) {
+  private static IStorageService getStorageService(String storageType) {
     String storageKey = PropertiesCache.getInstance().getProperty(JsonKey.ACCOUNT_NAME);
     String storageSecret = PropertiesCache.getInstance().getProperty(JsonKey.ACCOUNT_KEY);
     return getStorageService(storageType, storageKey, storageSecret);
   }
 
-  private static IStorageService getAnalyticsStorageService(CloudStorageType storageType) {
-    String storageKey = PropertiesCache.getInstance().getProperty(JsonKey.ANALYTICS_ACCOUNT_NAME);
-    String storageSecret = PropertiesCache.getInstance().getProperty(JsonKey.ANALYTICS_ACCOUNT_KEY);
-    return getStorageService(storageType, storageKey, storageSecret);
-  }
-
   private static IStorageService getStorageService(
-      CloudStorageType storageType, String storageKey, String storageSecret) {
-    String compositeKey = storageType.getType() + "-" + storageKey;
+      String storageType, String storageKey, String storageSecret) {
+    String compositeKey = storageType + "-" + storageKey;
     if (storageServiceMap.containsKey(compositeKey)) {
       return storageServiceMap.get(compositeKey);
     }
     synchronized (CloudStorageUtil.class) {
       StorageConfig storageConfig =
-          new StorageConfig(storageType.getType(), storageKey, storageSecret);
+          new StorageConfig(storageType, storageKey, storageSecret);
       IStorageService storageService = StorageServiceFactory.getStorageService(storageConfig);
       storageServiceMap.put(compositeKey, storageService);
     }
@@ -122,7 +73,7 @@ public class CloudStorageUtil {
   }
 
   public static String getUri(
-      CloudStorageType storageType, String container, String prefix, boolean isDirectory) {
+      String storageType, String container, String prefix, boolean isDirectory) {
     IStorageService storageService = getStorageService(storageType);
     return storageService.getUri(container, prefix, Option.apply(isDirectory));
   }

25704ae2bd329ce3e4ccf3bbcfaf362f1a77794d
aimansharief <110388201+aimansharief@users.noreply.github.com>
2022-11-21 15:46:03 +0530

    feat : add Cname changes

--- service/app/filters/ResponseFilter.scala
+++ service/app/filters/ResponseFilter.scala
@@ -6,7 +6,7 @@ import org.sunbird.common.models.util.JsonKey
 import play.api.http.HttpEntity.Strict
 import play.api.mvc.{Filter, RequestHeader, Result}
 import org.sunbird.common.models.util.ProjectUtil.getConfigValue
-
+import play.api.Logger.logger
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -15,16 +15,14 @@ class ResponseFilter  @Inject()(implicit val mat: Materializer, ec: ExecutionCon
 
   override def apply(nextFilter: (RequestHeader) => Future[Result])(rh: RequestHeader) =
     nextFilter(rh) flatMap { result =>
-      println("Executing ResponseFilter (plain filter) in course-service...")
       if (null != result.body && !result.body.isKnownEmpty){
         val contentType = result.body.contentType
         val updatedBody = result.body.consumeData.map { x =>
           val y = x.utf8String.replaceAll(JsonKey.CLOUD_STORE_BASE_PATH, getConfigValue(JsonKey.CLOUD_STORE_BASE_PATH_VALUE))
-          println("updated body: " + y)
+          logger.info("updated body: " + y)
           y
         }
         updatedBody map { x =>
-          println("inside updatedBody map. ")
           result.copy(body = Strict(ByteString(x), contentType))
         }
       } else {

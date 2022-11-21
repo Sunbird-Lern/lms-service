9a358c904cadb8c053bb2aa275a02481d3edc0af
aimansharief <110388201+aimansharief@users.noreply.github.com>
2022-11-21 14:38:33 +0530

    feat : add Cname changes

--- service/app/filters/ResponseFilter.scala
+++ service/app/filters/ResponseFilter.scala
@@ -16,15 +16,19 @@ class ResponseFilter  @Inject()(implicit val mat: Materializer, ec: ExecutionCon
   override def apply(nextFilter: (RequestHeader) => Future[Result])(rh: RequestHeader) =
     nextFilter(rh) flatMap { result =>
       println("Executing ResponseFilter (plain filter) in course-service...")
-      val contentType = result.body.contentType
-      val updatedBody = result.body.consumeData.map { x =>
-        val y = x.utf8String.replaceAll(JsonKey.CLOUD_STORE_BASE_PATH, getConfigValue(JsonKey.CLOUD_STORE_BASE_PATH_VALUE))
-        println("updated body: " + y)
-        y
-      }
-      updatedBody map { x =>
-        println("inside updatedBody map. ")
-        result.copy(body = Strict(ByteString(x), contentType))
+      if (null != result.body && !result.body.isKnownEmpty){
+        val contentType = result.body.contentType
+        val updatedBody = result.body.consumeData.map { x =>
+          val y = x.utf8String.replaceAll(JsonKey.CLOUD_STORE_BASE_PATH, getConfigValue(JsonKey.CLOUD_STORE_BASE_PATH_VALUE))
+          println("updated body: " + y)
+          y
+        }
+        updatedBody map { x =>
+          println("inside updatedBody map. ")
+          result.copy(body = Strict(ByteString(x), contentType))
+        }
+      } else {
+        Future(result)
       }
     }
 }

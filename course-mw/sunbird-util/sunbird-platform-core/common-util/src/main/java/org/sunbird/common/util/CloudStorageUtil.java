0b9e87f128609dfdee61107e0c3cd22f5ab03799
Anil Gupta <ani2824@gmail.com>
2023-07-13 11:29:57 +0530

    Issue #LR-627 merge: OCI CSP changes from release-5.0.2 to release-5.1.0 (#576)

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/CloudStorageUtil.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/util/CloudStorageUtil.java
@@ -21,7 +21,7 @@ public class CloudStorageUtil {
   private static final int STORAGE_SERVICE_API_RETRY_COUNT = 3;
 
   private static final Map<String, BaseStorageService> storageServiceMap = new HashMap<>();
-
+  
   public static String upload(
       String storageType, String container, String objectKey, String filePath) {
 
@@ -51,26 +51,31 @@ public class CloudStorageUtil {
             Some.apply("r"), Some.apply("application/pdf"));
   }
 
-  private static BaseStorageService getStorageService(String storageType) {
-    String storageKey = PropertiesCache.getInstance().getProperty(JsonKey.ACCOUNT_NAME);
-    String storageSecret = PropertiesCache.getInstance().getProperty(JsonKey.ACCOUNT_KEY);
-    return getStorageService(storageType, storageKey, storageSecret);
-  }
 
+
+	private static BaseStorageService getStorageService(String storageType) {
+		String storageKey = PropertiesCache.getInstance().getProperty(JsonKey.ACCOUNT_NAME);
+		String storageSecret = PropertiesCache.getInstance().getProperty(JsonKey.ACCOUNT_KEY);
+		return getStorageService(storageType, storageKey, storageSecret);
+	}
+	  
   private static BaseStorageService getStorageService(
-      String storageType, String storageKey, String storageSecret) {
-    String compositeKey = storageType + "-" + storageKey;
-    if (storageServiceMap.containsKey(compositeKey)) {
-      return storageServiceMap.get(compositeKey);
-    }
-    synchronized (CloudStorageUtil.class) {
-      StorageConfig storageConfig =
-          new StorageConfig(storageType, storageKey, storageSecret);
-      BaseStorageService storageService = StorageServiceFactory.getStorageService(storageConfig);
-      storageServiceMap.put(compositeKey, storageService);
-    }
-    return storageServiceMap.get(compositeKey);
-  }
+	      String storageType, String storageKey, String storageSecret) {
+	    String compositeKey = storageType + "-" + storageKey;
+	    scala.Option<String> storageEndpoint = scala.Option.apply(PropertiesCache.getInstance().getProperty(JsonKey.ACCOUNT_ENDPOINT));
+	    scala.Option<String> storageRegion = scala.Option.apply("");	    
+	    if (storageServiceMap.containsKey(compositeKey)) {
+	      return storageServiceMap.get(compositeKey);
+	    }
+	    synchronized (CloudStorageUtil.class) {
+	      StorageConfig storageConfig =
+	      new StorageConfig(storageType, storageKey, storageSecret,storageEndpoint,storageRegion);
+	      BaseStorageService storageService = StorageServiceFactory.getStorageService(storageConfig);
+	      storageServiceMap.put(compositeKey, storageService);
+	    }
+	    return storageServiceMap.get(compositeKey);
+   }
+  
 
   private static int getTimeoutInSeconds() {
     String timeoutInSecondsStr = ProjectUtil.getConfigValue(JsonKey.DOWNLOAD_LINK_EXPIRY_TIMEOUT);

f57bca011af6a6bd4fc70505a5eb7a5c08ae7e0f
Amit Priyadarshi <darshi.1983@gmail.com>
2024-07-01 13:55:33 +0530

    Issue #LR-814 merge: merge release-7.0.0 to master (#620)

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/util/ContentUtil.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/util/ContentUtil.java
@@ -129,7 +129,7 @@ public final class ContentUtil {
     Map<String, String> headers = new HashMap<>();
     try {
       String fieldsStr = StringUtils.join(fields, ",");
-      String baseContentreadUrl = ProjectUtil.getConfigValue(JsonKey.EKSTEP_BASE_URL) + PropertiesCache.getInstance().getProperty(JsonKey.CONTENT_READ_URL) + courseId + "?fields=" + fieldsStr;
+      String baseContentreadUrl = ProjectUtil.getConfigValue(JsonKey.CONTENT_SERVICE_BASE_URL) + PropertiesCache.getInstance().getProperty(JsonKey.CONTENT_READ_URL) + courseId + "?fields=" + fieldsStr;
       headers.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
       headers.put(JsonKey.AUTHORIZATION, PropertiesCache.getInstance().getProperty(JsonKey.EKSTEP_AUTHORIZATION));
 
@@ -183,7 +183,7 @@ public final class ContentUtil {
   public static boolean updateCollection(RequestContext requestContext, String collectionId, Map<String, Object> data) {
     String response = "";
     try {
-      String contentUpdateBaseUrl = ProjectUtil.getConfigValue(JsonKey.LEARNING_SERVICE_BASE_URL);
+      String contentUpdateBaseUrl = ProjectUtil.getConfigValue(JsonKey.CONTENT_SERVICE_BASE_URL);
       Request request = new Request();
       request.put("content", data);
       response =
